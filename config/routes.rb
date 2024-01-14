4592e182fee259e53d4c1af4b12d7daef4d18eb9
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-01-14 06:46:30 +0000

    [add]モデルなどの各種ファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+  get '/top' => 'homes#top'
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20240114061029_addbody_tobooks.rb
@@ -0,0 +1,4 @@
+class AddbodyTobooks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
