faf3089a10afb974c301b772bb89b25466c489ca
EC2 Default User <ec2-user@ip-172-31-5-92.ap-northeast-1.compute.internal>
2024-01-15 09:27:05 +0000

    [add]indexページに表追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
     book = Book.new(book_params)
@@ -11,6 +8,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
